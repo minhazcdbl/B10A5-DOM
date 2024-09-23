bb6cdcc24d6cf21cbe0cebba0795c1a63ee3af94
minhazcdbl <minhaz.cdbl@gmail.com>
2024-09-23 08:51:43 +0600

    sixth commit

---
+++ js/blog.js
@@ -0,0 +1,7 @@
+document.getElementById('blog-entry')
+    .addEventListener('click', function(){
+        
+            window.location.href = './home.html';
+       
+});
+
---
+++ js/mainp.js
@@ -0,0 +1,7 @@
+document.getElementById('main-entry')
+    .addEventListener('click', function(){
+        
+            window.location.href = './index.html';
+       
+});
+
